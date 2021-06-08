800dca71bfb5048e3b80395da9717a84d8e94639
palina135 <po_lina13579@yahoo.com>
2021-06-08 23:36:15 +0300

    kkkk

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
-console.log('hello everyone')
+let message = 'hello';
+console.log('hello');
+
+let age=25, name='female';
+age=name;
+alert(age);
+
+let value = true;
+alert(typeof value); // boolean
+
+value = String(value); // теперь value это строка "true"
+alert(typeof value); // string
